7145cd2a5853ed4a7e2f341f30710241beef9a82
JKearnsl <76239707+JKearnsl@users.noreply.github.com>
2022-04-17 13:36:28 +0300

    Add files via upload

---
+++ tcpProject/tcpProject.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+#include "Server.h"
+#include "windows.h"
+
+
+std::string dirFinder(std::string dir)
+{
+	WIN32_FIND_DATA FindFileData;
+	HANDLE hf;
+
+	std::wstring wstr = std::wstring(dir.begin(), dir.end());
+	LPCWSTR swDir = wstr.c_str();
+
+	hf = FindFirstFile(swDir, &FindFileData);
+
+	std::string result;
+
+	if (hf != INVALID_HANDLE_VALUE)
+	{
+		std::wstring buffFilename;
+		do
+		{
+			buffFilename = FindFileData.cFileName;
+			std::string strFilename(buffFilename.begin(), buffFilename.end());
+
+			result += strFilename + "\n";
+
+		} while (FindNextFile(hf, &FindFileData) != 0);
+		FindClose(hf);
+	}
+    return result;
+}
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    startServer(dirFinder, "127.0.0.1", "27015");
+    return 0;
+}
